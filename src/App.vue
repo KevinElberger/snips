6a5c829130a27faad5d74f4f84f2bb81e6625fc5
Kevin <kevelberger@gmail.com>
2018-06-22 18:57:38 -0700

    Add New button and Languages to Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
-    <h1>This is not an IDE!</h1>
     <sidebar />
   </div>
 </template>
 
 <style>
-html {
-  background-color: #FCFCFC;
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 #app {
+  width: 100%;
+  height: 100%;
+  color: #2c3e50;
+  text-align: center;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
 
 <script>
   import Sidebar from './components/Sidebar/Sidebar.vue';
 
-
-export default {
-  name: 'App',
-  components: {
-    Sidebar
-  }
-};
+  export default {
+    name: 'App',
+    components: {
+      Sidebar
+    }
+  };
 </script>
