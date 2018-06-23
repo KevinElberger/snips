1af736c81a95d2a6b70ee592a45ddc9ceba3a4c3
Kevin <kevelberger@gmail.com>
2018-06-22 23:30:04 -0700

    Add Store to App for state management

--- src/app.js
+++ src/app.js
@@ -2,7 +2,21 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
 
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    snippets: []
+  },
+  mutations: {
+    addSnippet (state, payload) {
+      state.snippets.push(payload);
+    }
+  }
+});
+
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 });
