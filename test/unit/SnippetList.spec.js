7a8e0602bcf44e74816110e832edd0564ad27499
Kevin <kevelberger@gmail.com>
2018-07-03 13:05:30 -0700

    Add default message for SnippetList

--- test/unit/SnippetList.spec.js
+++ test/unit/SnippetList.spec.js
@@ -18,4 +18,16 @@ describe('SnippetList', () => {
 
     expect(wrapper.findAll('a')).to.have.lengthOf(mockSnippets.length);
   });
+
+  it('renders the default message if no snippets exist', () => {
+    const wrapper = shallowMount(SnippetList, {
+      computed: {
+        snippets: () => {
+          return [];
+        }
+      }
+    });
+    
+    expect(wrapper.findAll('.no-snippets')).to.have.lengthOf(1);
+  });
 });
